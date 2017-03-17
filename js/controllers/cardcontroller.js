82dbe834c509b268498a9533a5ed74466130d855
Robin Zigmond <robinzig@hotmail.com>
2017-03-17 23:21:25 +0000

    added links to other cards on cardpages. Some other minor changes, most to do with getting icons to display correctly.

--- js/controllers/cardcontroller.js
+++ js/controllers/cardcontroller.js
@@ -25,6 +25,10 @@ angular.module("RouteControllerCard", [])
 				if ($scope.thisCard.set == "IntrigueSecondEd") {
 					$scope.thisCard.set = "Intrigue (2nd Edition)";
 				}
+
+				// pluralise "Type(s)" correctly depending on number of types card has:
+				if ($scope.thisCard.types.length>1) $scope.typeOrTypes = "Types";
+				else $scope.typeOrTypes = "Type";
 				
 				
 				// to display the card's cost with the appropriate icons - definte an array of objects, one for each icon needed.
@@ -52,6 +56,7 @@ angular.module("RouteControllerCard", [])
 					$scope.costIcons.push({path: "images/"+$scope.thisCard.costInDebt+"debt.png", text: $scope.thisCard.costInDebt+"debt"});
 				}
 
+				// replace relevant text on cards and in card explanations with the corresponding icons
 				$http.get("js/data/icons.json")
 					.then (function(results) {
 						$scope.iconGlossary = results.data;
@@ -64,6 +69,32 @@ angular.module("RouteControllerCard", [])
 							$scope.iconGlossary[icon].text).join($scope.iconGlossary[icon].html);
 						}
 					});
+
+				// make each card name into a link
+				for (card in $scope.cardList) {
+					// don't want links for references to the same card (looks too busy and adds nothing):
+					if ($scope.cardList[card].name == $scope.thisCard.name) continue;
+
+					var name = $scope.cardList[card].name;
+					// there is an issue with apostrophes in card names (eg. King's Court) which breaks the links
+					// these need a alternative string to escape them inside the HTML
+					var linkName = name.replace("'", "&#39");
+
+					// ugly string construction to get correct HTML for link:
+					var linkHTML = "<a href='/cards/" + linkName + "' alt='" + name + "'>" + name + "</a>";
 					
+					// only replace first reference to a card with link within the same section, to avoid visual clutter
+					// (so use .replace instead of .split then .join)
+					$scope.thisCard.textAboveLine = $scope.thisCard.textAboveLine.replace(name, linkHTML);
+					$scope.thisCard.textBelowLine = $scope.thisCard.textBelowLine.replace(name, linkHTML);
+					$scope.thisCard.discussion = $scope.thisCard.discussion.replace(name, linkHTML);
+				}
+				/* note that there is a small issue remaining with the above code: where one card's name is a substring of another's
+				(Village inside Mining Village etc, Gold inside Fool's Gold), sometimes the wrong one of the "inner" and "outer" card
+				gets the link element. This can largely be solved by careful ordering of cards in the json array (putting the "outer"
+				card first always) - but it is still a problem when an "outer" card references itself (there should be no link at all, 
+				but	the result is a link to the "inner" card. I'm not yet sure of the best solution to this issue. */
+
 			});
+				
 	});
