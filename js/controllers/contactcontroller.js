cb57514f65c52473291d065d8efeb581e335557d
Robin Zigmond <robinzig@hotmail.com>
2017-03-31 22:03:27 +0100

    added very basic validation to form, and slightly layout changes

--- js/controllers/contactcontroller.js
+++ js/controllers/contactcontroller.js
@@ -1,4 +1,14 @@
 angular.module("RouteControllerContact", [])
 	.controller("ContactController", function($scope) {
-
+		$scope.submitForm = function() {
+			if ($scope.contactForm.$valid) {
+				alert("Thank you for your feedback!");
+				$scope.name = "";
+				$scope.email="";
+				$scope.feedback="";
+			}
+			else {
+				alert("Invalid input - please try again.")
+			}
+		}
 	});
