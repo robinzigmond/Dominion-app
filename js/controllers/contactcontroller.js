7d6f6f8019b9fd7ae5c83f11e7b238d0f694a7b4
Robin Zigmond <robinzig@hotmail.com>
2017-04-02 13:21:05 +0100

    improved validation of contact form, with more detailed feedback to user

--- js/controllers/contactcontroller.js
+++ js/controllers/contactcontroller.js
@@ -1,14 +1,22 @@
 angular.module("RouteControllerContact", [])
 	.controller("ContactController", function($scope) {
+		$scope.name = "";
+		$scope.email="";
+		$scope.feedback="";
+		$scope.nameMissing = false; 
+		$scope.invalidEmail = false;
+		$scope.noFeedback = false;
 		$scope.submitForm = function() {
+			console.log($scope.contactForm.name);
+			console.log($scope.contactForm.email);
+			$scope.nameMissing = !($scope.contactForm.name.$valid);
+			$scope.invalidEmail = !($scope.contactForm.email.$valid);
+			$scope.noFeedback = !($scope.contactForm.feedback.$valid);	
 			if ($scope.contactForm.$valid) {
-				alert("Thank you for your feedback!");
+				alert("Thank you for your comments!");
 				$scope.name = "";
 				$scope.email="";
 				$scope.feedback="";
-			}
-			else {
-				alert("Invalid input - please try again.")
-			}
+			}		
 		}
 	});
